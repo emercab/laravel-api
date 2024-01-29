b2acfa828506faf2565c5c66b78f80b4f09d086f
Emerson Cabrera <info@emersoncabrera.com>
2024-01-29 09:04:50 -0500

    Api auth ready

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+  public function register(Request $request)
+  {
+    $request->validate([
+      'name' => 'required|string',
+      'email' => 'required|string|email|unique:users',
+      'password' => 'required|string|confirmed'
+    ]);
+
+    $user = new User([
+      'name' => $request->name,
+      'email' => $request->email,
+      'password' => bcrypt($request->password)
+    ]);
+
+    $user->save();
+
+    return response()->json([
+      'message' => 'Successfully created user!'
+    ], 201);
+  }
+
+  public function login(Request $request)
+  {
+    $request->validate([
+      'email' => 'required|string|email',
+      'password' => 'required|string'
+    ]);
+
+    $credentials = request(['email', 'password']);
+
+    if (!Auth::attempt($credentials))
+      return response()->json([
+        'message' => 'Unauthorized'
+      ], 401);
+
+    $user = $request->user();
+    $tokenResult = $user->createToken('token');
+    $token = $tokenResult->token;
+    $token->save();
+
+    return response()->json([
+      'access_token' => $tokenResult->accessToken,
+      'token_type' => 'Bearer'
+    ]);
+  }
+
+  public function get_user(Request $request)
+  {
+    return response()->json($request->user());
+  }
+}
